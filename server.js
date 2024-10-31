0465f26a7f02a59700d9b970629e6dec848213e0
Nguyễn Thị Minh Nguyệt <145785457+MinhNguyet-Love@users.noreply.github.com>
2024-10-31 20:44:46 +0700

    aaaaa

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Kết nối tới MongoDB
-mongoose.connect('mongodb+srv://nguyet100147:5SxyWLOvFg3m3sxH@datafellow4u.igqq4.mongodb.net/tour', {
+// Kết nối tới MongoDB bằng biến môi trường
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB:', err));
 
 // Endpoint để lấy danh sách tin tức du lịch
 app.get('/travel-news', async (req, res) => {
