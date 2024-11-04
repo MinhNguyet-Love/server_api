84e6dc2c5bae90271950e726091e4e24aebc01bf
Nguyễn Thị Minh Nguyệt <145785457+MinhNguyet-Love@users.noreply.github.com>
2024-11-04 17:54:06 +0700

    ooo

--- models/thongbao.js
+++ models/thongbao.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const travelNewsSchema1 = new mongoose.Schema({
   tieude: { type: String, required: true },
   title: { type: String, required: true },
-  date: { type: String, required: true }, // hoặc có thể sử dụng Date type
+  date: { type: String, required: true }, // You could also use Date type if you prefer
 });
 
+// Create the model using the schema
 const TravelNews1 = mongoose.model('thongbao', travelNewsSchema1);
 
+// Export the model
 module.exports = TravelNews1;
