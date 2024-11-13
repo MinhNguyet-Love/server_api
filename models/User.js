312acf35b75057ddbd829a3e13707c858ba69314
Nguyễn Thị Minh Nguyệt <145785457+MinhNguyet-Love@users.noreply.github.com>
2024-11-13 13:47:44 +0700

    13/11

--- models/User.js
+++ models/User.js
@@ -1,25 +1,8 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
-// Định nghĩa Schema cho User
 const userSchema = new mongoose.Schema({
-  email: { 
-    type: String, 
-    required: true,
-    unique: true, // Đảm bảo email là duy nhất
-    match: [/.+@.+\..+/, 'Địa chỉ email không hợp lệ'] // Kiểm tra định dạng email
-  },
-  name: { 
-    type: String, 
-    required: true 
-  },
-  role: { 
-    type: String, 
-    required: true,
-    enum: ['Admin', 'User', 'Moderator'] // Giới hạn các giá trị có thể cho role (có thể điều chỉnh)
-  },
-}, { timestamps: true }); // Thêm thời gian tạo và cập nhật tự động
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-// Tạo Model User
-const User = mongoose.model("User", userSchema);
-
-export default User;
+module.exports = mongoose.model('User', userSchema);
